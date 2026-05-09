ddc5496e0cfdd129d272ff1c586c293d13904e1f
Rubal-code <rubal3232@gmail.com>
2026-05-09 23:50:12 +0530

    sentence transformers

--- backend/services/ats.py
+++ backend/services/ats.py
@@ -1,58 +1,17 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
-import re
 
+model = SentenceTransformer(
+    'all-MiniLM-L6-v2'
+)
 
-def preprocess_text(text):
-    """
-    Clean and preprocess text
-    """
+def calculate_ats_score(resume, jd):
 
-    text = text.lower()
+    embeddings = model.encode([resume, jd])
 
-    # Remove special characters
-    text = re.sub(r'[^a-zA-Z0-9\s]', ' ', text)
-
-    # Remove extra spaces
-    text = re.sub(r'\s+', ' ', text).strip()
-
-    return text
-
-
-def calculate_ats_score(resume_text, job_description):
-    """
-    Calculate ATS similarity score between
-    resume and job description
-    """
-
-    # Check empty input
-    if not resume_text or not job_description:
-        return 0
-
-    # Preprocess texts
-    resume_text = preprocess_text(resume_text)
-    job_description = preprocess_text(job_description)
-
-    # Create document list
-    documents = [resume_text, job_description]
-
-    # TF-IDF Vectorizer
-    tfidf = TfidfVectorizer(
-        stop_words='english',
-        ngram_range=(1, 2),
-        max_features=5000
-    )
-
-    # Convert text to vectors
-    tfidf_matrix = tfidf.fit_transform(documents)
-
-    # Calculate cosine similarity
     similarity = cosine_similarity(
-        tfidf_matrix[0:1],
-        tfidf_matrix[1:2]
+        [embeddings[0]],
+        [embeddings[1]]
     )
 
-    # Convert to percentage
-    score = round(float(similarity[0][0]) * 100, 2)
-
-    return score
+    return round(similarity[0][0] * 100, 2)
