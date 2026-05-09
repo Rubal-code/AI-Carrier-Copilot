b868d8a1674449aa57bf0e191d033e635120c370
Rubal-code <rubal3232@gmail.com>
2026-05-09 23:04:35 +0530

    now extracting the content and link backend with frontend

--- frontend/Home.py
+++ frontend/Home.py
@@ -1,4 +1,10 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st 
+
+from backend.services.parser import extract_text
 
 st.title("AI Resume Analyzer")
 
@@ -8,5 +14,12 @@ uploaded_file = st.file_uploader(
 )
 
 if uploaded_file is not None:
+    save_path = os.path.join("uploads", uploaded_file.name)
+    
+    with open(save_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
     st.success("File uploaded successfully!")
+
+    text=extract_text(save_path)
+    st.text_area("Extracted Text", value=text, height=300)
     
